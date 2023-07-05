33d338a2e470721fbeab21519702e7a9a6e9d3fd
renuvenu <renuvenu2222@gmail.com>
2023-07-05 22:21:21 +0530

    services and some integration done

--- src/app/floors-component/floors-component.component.ts
+++ src/app/floors-component/floors-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FloorsService } from '../floorsService.service';
+import { PersonService } from 'src/person.service';
 
 @Component({
   selector: 'app-floors-component',
@@ -6,5 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./floors-component.component.css'],
 })
 export class FloorsComponentComponent {
-  arr: Number[] = [1, 2, 3, 4, 5];
+  floors: any;
+  showLogin = false;
+  openDoor = false;
+  showButtons = false;
+  userId: string = '';
+  toFloor: Number = 1;
+  fromFloor: Number = 1;
+  reachedDestination = false;
+  isFull: any;
+  showCapacityExceededPopup = false
+  constructor(
+    private floorService: FloorsService,
+    private personService: PersonService
+  ) {}
+  ngOnInit() {
+    this.getFloors();
+  }
+
+  getFloors() {
+    this.floorService.getFloors().subscribe((val) => {
+      var res = val;
+      this.floors = res;
+      this.floors = this.floors.reverse();
+    });
+  }
+
+  enableLoginScreen(showLogin: boolean) {
+    this.personService.verifyCapacity().subscribe((val) => {
+      let res = val;
+      this.isFull = res;
+    });
+    if (this.isFull) {
+      this.showCapacityExceededPopup = true
+    } else {
+      this.showLogin = showLogin;
+      this.toFloor = this.fromFloor;
+    }
+  }
+
+  toggleScreenLogin(id: string) {
+    this.userId = id;
+    this.showLogin = false;
+    setTimeout(() => {
+      this.openDoor = true;
+      setTimeout(() => {
+        this.showButtons = true;
+      }, 1000);
+    }, 1000);
+  }
+
+  numberClicked(num: Number) {
+    this.openDoor = false;
+    this.showButtons = false;
+    // setTimeout(() => {
+    this.toFloor = num;
+    this.reachedDestination = true;
+    // }, 1000);
+  }
+
+  updateFromFloor(num: Number) {
+    this.fromFloor = num;
+  }
 }
