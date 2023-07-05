3606434890ea49507f4c83ffa7ea226d823b6271
Lavanya.T <Lavanya.T@nafinc.com>
2023-07-05 16:59:19 +0530

    solving conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,15 +10,15 @@ import { FloorComponent } from './floor/floor.component';
 import { FloorsComponentComponent } from './floors-component/floors-component.component';
 import { RegformComponent } from './regform/regform.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
+// import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { PopUpComponent } from './pop-up/pop-up.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -33,16 +33,16 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     // AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatInputModule,
+    // FormsModule,
+    // ReactiveFormsModule,
+    // MatButtonModule,
+    // MatInputModule,
     MatFormFieldModule,
-    MatCardModule,
+    // MatCardModule,
     MatIconModule,
     MatDialogModule, 
     MatDialogModule,
-    HttpClientModule,
+    // HttpClientModule,
 
   ],
   providers: [],
