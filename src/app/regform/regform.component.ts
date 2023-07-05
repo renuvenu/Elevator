4a4f907427ba978da36f0121bfeb6020bbf2045d
Keerthana0505 <keerthanajothi2002@gmail.com>
2023-07-05 19:45:00 +0530

    Updated the Person Registrarion

--- src/app/regform/regform.component.ts
+++ src/app/regform/regform.component.ts
@@ -17,6 +17,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RegformComponent {
   values:any
+  user_Id:any
 
   addRegForm = this.fb.group({
     name: ['', Validators.required],
@@ -36,25 +37,40 @@ export class RegformComponent {
     return this.addRegForm.get('contactNumber');
   }
 
-  onSubmit()
-  {
-    this.http
+  // onSubmit()
+  // {
+  //   this.http
 
-      .post(`https://localhost:7160/api/person`, this.addRegForm.value)
+  //     .post(`https://localhost:7160/api/person`, this.addRegForm.value)
 
+  //     .subscribe(data => {
+
+  //       this.values = data;
+
+  //        this.user_Id= this.values.userId;  
+
+
+  //     });
+  // }
+
+
+  onSubmit() {
+    this.http.post(`https://localhost:7160/api/person`, this.addRegForm.value)
       .subscribe(data => {
-
         this.values = data;
-
-        const userId1= this.values.userId;  
-
-        console.log(userId1);
-
+        this.user_Id = this.values.userId;
+        console.log(this.user_Id);
+        this.openDialog(this.user_Id);
       });
   }
-
-  openDialog() {
-    this.dialog.open(PopUpComponent);
+  openDialog(userId: any) {
+    const dialogRef = this.dialog.open(PopUpComponent, {
+      data: { userId: userId }
+    });
   }
+  
+  
+  
+  
   
 }
