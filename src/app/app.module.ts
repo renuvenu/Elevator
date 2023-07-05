ee50a0bdb6b9ed6b9b1c5a55c59da63dd9a52787
Amridhasri <amridhasrianand@gmail.com>
2023-07-05 16:53:06 +0530

    user login changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleLiftComponent } from './single-lift/single-lift.component';
 import { FloorComponent } from './floor/floor.component';
 import { FloorsComponentComponent } from './floors-component/floors-component.component';
-
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     SingleLiftComponent,
     FloorComponent,
-    FloorsComponentComponent
+    FloorsComponentComponent,
+    UserDetailComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatCardModule,MatInputModule,MatButtonModule,MatIconModule,FormsModule, ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
