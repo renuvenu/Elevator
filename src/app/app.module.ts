d38e0ca0b7b5407335d5f01a1265c38c94b441d9
Lavanya.T <Lavanya.T@nafinc.com>
2023-07-05 13:28:02 +0530

    Reg form code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleLiftComponent } from './single-lift/single-lift.component';
 import { FloorComponent } from './floor/floor.component';
 import { FloorsComponentComponent } from './floors-component/floors-component.component';
+import { RegformComponent } from './regform/regform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     SingleLiftComponent,
     FloorComponent,
-    FloorsComponentComponent
+    FloorsComponentComponent,
+    RegformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/regform/regform.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-regform',
+  templateUrl: './regform.component.html',
+  styleUrls: ['./regform.component.css'],
+})
+export class RegformComponent {
+  name: any;
+  officename: any;
+  contactno: any;
+
+  addRegForm = this.fb.group({
+    name: ['', Validators.required],
+    officename: ['', Validators.required],
+    contactno: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {}
+}
