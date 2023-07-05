4deded05c3d3059cd7a51c78d4e264b5b7b4c49e
renuvenu <renuvenu2222@gmail.com>
2023-07-05 11:40:41 +0530

    added floors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleLiftComponent } from './single-lift/single-lift.component';
+import { FloorComponent } from './floor/floor.component';
+import { FloorsComponentComponent } from './floors-component/floors-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleLiftComponent
+    SingleLiftComponent,
+    FloorComponent,
+    FloorsComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/floors-component/floors-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floors-component',
+  templateUrl: './floors-component.component.html',
+  styleUrls: ['./floors-component.component.css'],
+})
+export class FloorsComponentComponent {
+  arr: Number[] = [1, 2, 3, 4, 5];
+}
