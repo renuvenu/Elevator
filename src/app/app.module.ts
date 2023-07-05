460b237a2fc58f05cfc5261f0d04a7c2b7b35e65
Lavanya.T <Lavanya.T@nafinc.com>
2023-07-05 17:11:55 +0530

    con res

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,48 @@
 import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { SingleLiftComponent } from './single-lift/single-lift.component';
-// import { FloorComponent } from './floor/floor.component';
-// import { FloorsComponentComponent } from './floors-component/floors-component.component';
+import { SingleLiftComponent } from './single-lift/single-lift.component';
+import { FloorComponent } from './floor/floor.component';
+import { FloorsComponentComponent } from './floors-component/floors-component.component';
 import { RegformComponent } from './regform/regform.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatCardModule } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { PopUpComponent } from './pop-up/pop-up.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-// import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // SingleLiftComponent,
-    // FloorComponent,
-    // FloorsComponentComponent,
+    SingleLiftComponent,
+    FloorComponent,
+    FloorsComponentComponent,
     RegformComponent,
     PopUpComponent
   ],
   imports: [
-    // BrowserModule,
-    // AppRoutingModule,
-    // BrowserAnimationsModule,
-    // FormsModule,
-    // ReactiveFormsModule,
-    // MatButtonModule,
-    // MatInputModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
     MatFormFieldModule,
-    // MatCardModule,
+    MatCardModule,
     MatIconModule,
     MatDialogModule, 
     MatDialogModule,
-    // HttpClientModule,
+    HttpClientModule,
 
   ],
   providers: [],
