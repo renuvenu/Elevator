36b5d37600572bc91d1fb456354c1fdb385c38d7
elanchikk <ellanchikkumar@gmail.com>
2023-07-06 17:28:54 +0530

    weight limit popup

---
+++ src/app/weight-limit-exceeded-popup/weight-limit-exceeded-popup.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-weight-limit-exceeded-popup',
+  templateUrl: './weight-limit-exceeded-popup.component.html',
+  styleUrls: ['./weight-limit-exceeded-popup.component.css'],
+})
+export class WeightLimitExceededPopupComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
+  goToLift() {
+    this.router.navigate(['/lift']);
+  }
+
+  // ngOnInit() {
+  //   this.dialog.open(WeightLimitExceededPopupComponent, {});
+  // }
+}
